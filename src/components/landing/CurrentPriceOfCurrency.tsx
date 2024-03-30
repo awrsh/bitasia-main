c10413e5ef13e58afab8abeac3526b8ca900392a
developer <mahdilove279@gmail.com>
2024-03-30 09:17:26 +0330

    page commission

--- src/components/landing/CurrentPriceOfCurrency.tsx
+++ src/components/landing/CurrentPriceOfCurrency.tsx
@@ -6,10 +6,14 @@ import { StyledTableCell, StyledTableRow } from '@/helpers/utils/mui'
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 import Link from 'next/link';
 
-const CurrentPriceOfCurrency = () => {
+const CurrentPriceOfCurrency = ({ showTitle = true }: { showTitle?: boolean }) => {
     return (
         <div className='mt-14'>
-            <h1 className='text-2xl text-white font-extrabold pb-10'>قیمت لحظه ای ارز</h1>
+            {
+                showTitle ?
+                    <h1 className='text-2xl text-white font-extrabold pb-10'>قیمت لحظه ای ارز</h1>
+                    : null
+            }
             <Table header={["نام‌ارز", "قیمت(تتر)", "قیمت(تومان)", "قیمت(تغیرات)", "نمودار", ""]}>
                 {currentPriceOfCurrency.map((currency, idx) => (
                     <StyledTableRow key={idx}>
@@ -18,8 +22,8 @@ const CurrentPriceOfCurrency = () => {
                         <StyledTableCell align='center'>{currency.price_toman}</StyledTableCell>
                         <StyledTableCell align='center'>{currency.percent}</StyledTableCell>
                         <StyledTableCell width={200} className='!min-w-[120px]' align='center'>
-                            <Sparklines  data={[0, 5, 20, 25, 20, 15, 10, 5, 0]}>
-                                <SparklinesLine style={{height:"20px"}} color={currency.percent < 0 ? "#20c997" : "#dc3545"} />
+                            <Sparklines data={[0, 5, 20, 25, 20, 15, 10, 5, 0]}>
+                                <SparklinesLine style={{ height: "20px" }} color={currency.percent < 0 ? "#20c997" : "#dc3545"} />
                             </Sparklines>
                         </StyledTableCell>
                         <StyledTableCell width={200} align='center'>
