0bd3ed0c0b51799414b461b286f9b68e2b025f61
awrsh <arashzarandi@gmail.com>
2024-04-14 14:41:42 +0330

    update key landing

--- src/components/landing/Options.tsx
+++ src/components/landing/Options.tsx
@@ -8,11 +8,9 @@ const Options = () => {
       <h1 className=' text-center after:-translate-x-1/2 font-extrabold  text-[#407bff] text-3xl relative after:absolute after:w-[300px] after:left-1/2 after:-bottom-6 after:h-px after:bg-[#407bff]'>ورود به دنیای ارز دیجیتال با بیت آسیا</h1>
       <div className='flex flex-col lg:flex-row items-center justify-around gap-5 mt-10 lg:mt-24'>
         {options.map((option, idx) => (
-          <div>
-            <div key={idx}>
-              <option.icon className='mx-auto text-[#407bff]' size={40} />
-              <p className='mt-3  text-1xs text-mode'>{option.text}</p>
-            </div>
+          <div key={idx}>
+            <option.icon className='mx-auto text-[#407bff]' size={40} />
+            <p className='mt-3  text-1xs text-mode'>{option.text}</p>
           </div>
         ))}
       </div>
